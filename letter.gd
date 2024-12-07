d10c6f40370d5822fc740b1e37c22d83e05d0670
Daoud Clarke <daoud.clarke@gmail.com>
2024-12-07 15:13:33 +0000

    Add word search

--- letter.gd
+++ letter.gd
@@ -3,13 +3,21 @@
 class_name Letter extends Node2D
 
 @export var font: Font = ThemeDB.fallback_font;
+@export var font_size: int = 16
+@export var default_background_color: Color
+@export var highlighted_color: Color
 
-var letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[randi_range(0, 25)]
 
+var letter: String
+var status: int = 0
+var neighbours: Array[Letter] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if randf() < 0.7:
+		letter = "ETAONRISH"[randi_range(0, 8)]
+	else:
+		letter = "BCDFGJKLMPQUVWXYZ"[randi_range(0, 16)]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,6 +26,19 @@ func _process(delta: float) -> void:
 
 
 func _draw():
-	var size = font.get_string_size(letter)
-	draw_string(font, -0.5*size, letter)
+	var size = font.get_string_size(letter, 0, -1, font_size)
+	var color: Color
+	match status:
+		0: color = default_background_color
+		1: color = highlighted_color
+	draw_circle(Vector2.ZERO, 12, color)
+	draw_string(font, Vector2(-0.5*size.x, 0.3*size.y), letter, 0, -1, font_size, Color("black"))
 	
+
+func add_neighbour(neighbour: Letter):
+	neighbours.append(neighbour)
+
+
+func set_found():
+	status = 1
+	queue_redraw()
---
+++ word_search.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+var letter_scene = preload("res://letter.tscn")
+var letters: Dictionary = {}
+var word: String = ""
+
+var cells = {}
+
+const SIZE = Vector2i(10, 10)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in SIZE.x:
+		for j in SIZE.y:
+			var letter = letter_scene.instantiate()
+			letter.position = Vector2(i * 25, j * 25)
+			add_child(letter)
+			if letter.letter in letters:
+				letters[letter.letter].append(letter)
+			else:
+				letters[letter.letter] = [letter]
+			cells[Vector2i(i, j)] = letter
+	
+	for i in SIZE.x:
+		for j in SIZE.y:
+			var letter: Letter = cells[Vector2i(i, j)]
+			if i > 0:
+				letter.add_neighbour(cells[Vector2i(i-1, j)])
+			if j > 0:
+				letter.add_neighbour(cells[Vector2i(i, j-1)])
+			if i < SIZE.x - 1:
+				letter.add_neighbour(cells[Vector2i(i+1, j)])
+			if j < SIZE.y - 1:
+				letter.add_neighbour(cells[Vector2i(i, j+1)])
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		var s = event.as_text_keycode()
+		word += s
+		var letters = letters[word[0]]
+		for letter in letters:
+			for c in word.substr(1):
+				var new_letters = []
+				if s in letters:
+					var found = letters[s]
+					for f in found:
+						f.set_found()
