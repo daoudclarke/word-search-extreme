fd9270e8123d626f9ec13d3908fdd7ae7f760d88
Daoud Clarke <daoud.clarke@gmail.com>
2024-12-11 20:04:15 +0000

    Try a slightly larger resolution

--- word_search.gd
+++ word_search.gd
@@ -50,7 +50,7 @@ const WILDCARD_LOCATIONS = {
 func new_letter(location: Vector2i):
 	var letter: Letter = letter_scene.instantiate()
 	letter.location = location
-	letter.position = Vector2(14 * location.x, 15*location.y)
+	letter.position = Vector2(13 * location.x, 14*location.y)
 
 	add_child(letter)
 
