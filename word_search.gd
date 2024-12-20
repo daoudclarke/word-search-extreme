7ed7773a1c81373ce30a5e1ccac5167a7fb6f142
Ismaeel Clarke <daoud.clarke@gmail.com>
2024-12-20 19:34:51 +0000

    WIP: implement depth first search

--- word_search.gd
+++ word_search.gd
@@ -5,32 +5,50 @@ var letters: Dictionary = {}
 var word: String = ""
 
 var cells = {}
+var cell_letters = {}
 
 const SIZE = Vector2i(11, 11)
 
 var just = [1.0, 9.0/8.0, 5.0/4.0, 4.0/3.0, 3.0/2.0, 5.0/3.0, 15.0/8.0, 2.0]
-var pitches = [0, 2, 4, 1, 3, 5, 2, 4, 6, 3, 5, 7, 4, 6, 8]
+var pitches = [0, 2, 4, 1, 3, 5, 2, 4, 6, 3, 5, 7, 4, 6, 8, 5, 7, 9]
 
 var dictionary: Dictionary = {}
 var letter_sequences = []
+var big_words = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var delay = 0.0
-	var pitch = 0
-	for i in range((SIZE.x - 1) / 2 - 1, (SIZE.x - 1) / 2 + 2):
-		for j in range((SIZE.y - 1) / 2 - 1, (SIZE.y - 1) / 2 + 2):
-			new_letter(Vector2i(i, j), delay, pitches[pitch])
-			delay += 0.1
-			pitch += 1
-	
-	print("Cells", cells)
-	
 	var file = FileAccess.open("res://sowpods.txt", FileAccess.READ)
 	while not file.eof_reached():
 		var line = file.get_line()
 		dictionary[line] = null
+		if len(line) == 9:
+			big_words.append(line)
+
+	var start_word = big_words[randi_range(0, len(big_words) - 1)]
+
+	var letter_index = {"": []}
+	for j in range((SIZE.x - 1) / 2 - 1, (SIZE.x - 1) / 2 + 2):
+		for k in range((SIZE.y - 1) / 2 - 1, (SIZE.y - 1) / 2 + 2):
+			letter_index[""].append(Vector2i(j, k))
+	
+	var sequences = get_letter_sequences(start_word, letter_index, 1)
+	sequences.shuffle()
+
+	var i = 0
+	for location in sequences[0]:
+		cell_letters[location] = start_word[i]
+		i += 1
+
+	var delay = 0.0
+	var count = 0
+	for j in range((SIZE.x - 1) / 2 - 1, (SIZE.x - 1) / 2 + 2):
+		for k in range((SIZE.y - 1) / 2 - 1, (SIZE.y - 1) / 2 + 2):
+			new_letter(Vector2i(j, k), delay, pitches[count % len(pitches)])
+			delay += 0.1
+			count += 1
+	
 	
 	#for key in cells:
 		#var i = key.x
@@ -62,14 +80,13 @@ func new_letter(location: Vector2i, delay: float, pitch: int):
 	
 	letter.play_note_delayed(pitch, delay)
 
-	if location in WILDCARD_LOCATIONS:
-		print("Wildcard", location)
-		letter.set_letter("")
+	if location in cell_letters:
+		letter.set_letter(cell_letters[location])
 
 	if letter.letter in letters:
-		letters[letter.letter].append(letter)
+		letters[letter.letter].append(location)
 	else:
-		letters[letter.letter] = [letter]
+		letters[letter.letter] = [location]
 	cells[location] = letter
 
 
@@ -83,14 +100,12 @@ const RELATIVES = [
 	Vector2i(0, -1), Vector2i(0, 1),
 	Vector2i(1, -1), Vector2i(1, 0), Vector2i(1, 1)]
 
-func get_neighbours(location: Vector2i) -> Array[Letter]:
-	var neighbours: Array[Letter] = []
+
+func get_neighbours(location: Vector2i) -> Array[Vector2i]:
+	var neighbours: Array[Vector2i] = []
 	for relative in RELATIVES:
 		var new_key = location + relative
-		print("New key",location, new_key)
-		if new_key in cells:
-			neighbours.append(cells[new_key])
-	print("Found neighbours", neighbours)
+		neighbours.append(new_key)
 	return neighbours
 
 
@@ -105,27 +120,7 @@ func _input(event: InputEvent) -> void:
 		if event.keycode == KEY_BACKSPACE:
 			word = word.substr(0, len(word) - 1)
 		elif event.keycode == KEY_ENTER:
-			if len(letter_sequences) > 0 and word in dictionary:
-				$FoundWords.text += word + "\n"
-				
-				letter_sequences.shuffle()
-				var pitch = 0
-				var delay = 0.0
-				for sequence in letter_sequences:
-					var i = 0
-					for letter: Letter in sequence:
-						if letter.used_status == 0:
-							letter.set_used_status(1)
-							for relative in RELATIVES:
-								var new_key = letter.location + relative
-								if new_key not in cells and new_key.x >= 0 and new_key.y >= 0 and new_key.x < SIZE.x and new_key.y < SIZE.y:
-									new_letter(new_key, delay, pitches[pitch])
-									pitch += 1
-									delay += 0.1
-						if letter.letter == "":
-							letter.set_letter(word[i])
-							i += 1
-
+				update_with_word(word, letter_sequences)
 				word = ""
 				$Word.text = word
 				return
@@ -137,31 +132,7 @@ func _input(event: InputEvent) -> void:
 		if len(word) == 0:
 			return
 		
-		if word[0] not in letters:
-			return
-
-		var letters = letters[word[0]]
-		letter_sequences = []
-		for letter in letters:
-			letter_sequences.append([letter])
-		
-		for c in word.substr(1):
-			var new_letter_sequences = []
-			for letter_sequence in letter_sequences:
-				var current: Letter = letter_sequence[-1]
-				var valid_neighbours = []
-				for neighbour in get_neighbours(current.location):
-					if neighbour.letter == c or neighbour.letter == "": 
-						var dup = false
-						for letter in letter_sequence:
-							if letter == neighbour:
-								dup = true
-						#var dup = neighbour in letter_sequence
-						if not dup:
-							var new_letter_sequence = letter_sequence.duplicate()
-							new_letter_sequence.append(neighbour)
-							new_letter_sequences.append(new_letter_sequence)
-			letter_sequences = new_letter_sequences
+		letter_sequences = get_letter_sequences(word, letters, 10)
 		
 		if len(letter_sequences) > 0:
 			var pitch = len(word) - 1
@@ -180,3 +151,106 @@ func _input(event: InputEvent) -> void:
 					letter.set_status(2)
 				else:
 					letter.set_status(1)
+
+
+func get_letter_sequences(word: String, letter_index: Dictionary, max_sequences: int) -> Array:
+	var sequences = []
+	var indexes = [0]
+	var sequence = []
+
+	var word_letters = []
+	for i in range(len(word)):
+		word_letters.append(letter_index.get(word[i], []) + letter_index.get("", []))
+
+	while true:
+		var i = len(indexes) - 1
+		var letter = word[i]
+		var letter_locations = word_letters[i]
+		var new_letter_location = letter_locations[indexes[i]]
+		var skip = false
+		var found = false
+		if i > 0:
+			var old_letter_location = sequence[len(sequence) - 1]
+			var neighbours = get_neighbours(old_letter_location)
+			if new_letter_location in neighbours:
+				sequence.append(new_letter_location)
+				found = true
+			if not found:
+				skip = true
+		else:
+			sequence.append(new_letter_location)
+			indexes.append(0)
+
+		if len(sequence) == len(word):
+			# We have completed a sequence
+			sequences.append(sequence.duplicate())
+			indexes.pop_back()
+			skip = true
+			if len(sequences) >= max_sequences:
+				break
+		if skip:
+			indexes[-1] += 1
+			if len(indexes) == 0:
+				break
+			while indexes[-1] >= len(word_letters[len(indexes) - 1]):
+				indexes.pop_back()
+				sequence.pop_back()
+				indexes[-1] += 1
+
+	print("Found sequences", sequences)
+	return sequences
+
+#func get_letter_sequences_old(word: String, letter_index: Dictionary) -> Array:
+	#var word_letters
+	#if word[0] in letter_index:
+		#word_letters = letter_index[word[0]] + letter_index[""]
+	#else:
+		#word_letters = letter_index[""]
+#
+	#var sequences = []
+	#for letter in word_letters:
+		#sequences.append([letter])
+	#
+	#print("Letter sequences", sequences)
+	#for c in word.substr(1):
+		#var new_letter_sequences = []
+		#for letter_sequence in sequences:
+			#var current: Letter = letter_sequence[-1]
+			#var valid_neighbours = []
+			#for neighbour in get_neighbours(current.location, cell_index):
+				#if neighbour.letter == c or neighbour.letter == "": 
+					#var dup = false
+					#for letter in letter_sequence:
+						#if letter == neighbour:
+							#dup = true
+					##var dup = neighbour in letter_sequence
+					#if not dup:
+						#var new_letter_sequence = letter_sequence.duplicate()
+						#new_letter_sequence.append(neighbour)
+						#new_letter_sequences.append(new_letter_sequence)
+		#sequences = new_letter_sequences
+	#return sequences
+
+
+
+func update_with_word(word: String, letter_sequences: Array):
+	if len(letter_sequences) > 0 and word in dictionary:
+		$FoundWords.text += word + "\n"
+	
+		letter_sequences.shuffle()
+		var pitch = 0
+		var delay = 0.0
+		for sequence in letter_sequences:
+			var i = 0
+			for letter: Letter in sequence:
+				if letter.used_status == 0:
+					letter.set_used_status(1)
+					for relative in RELATIVES:
+						var new_key = letter.location + relative
+						if new_key not in cells and new_key.x >= 0 and new_key.y >= 0 and new_key.x < SIZE.x and new_key.y < SIZE.y:
+							new_letter(new_key, delay, pitches[pitch % len(pitches)])
+							pitch += 1
+							delay += 0.1
+				if letter.letter == "":
+					letter.set_letter(word[i])
+					i += 1
